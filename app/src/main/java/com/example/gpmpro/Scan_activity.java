7df11e90f254fe8bb2b539de0da7a9f6f7272b4c
Mayur wayzade <mayurwayzade26@gmail.com>
2023-03-31 12:44:21 +0530

    scan activity added

--- app/src/main/java/com/example/gpmpro/Scan_activity.java
+++ app/src/main/java/com/example/gpmpro/Scan_activity.java
@@ -1,27 +1,22 @@
 package com.example.gpmpro;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
-import android.app.DatePickerDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class Scan_activity extends AppCompatActivity {
 
 
     WebView wb;
@@ -31,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scan);
         wb = findViewById(R.id.web);
         bonafide = findViewById(R.id.bonafide);
 
