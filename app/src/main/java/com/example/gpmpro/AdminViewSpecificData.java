051d4a9958a40aa368e9525a82b0ff2e58bf6fa9
Saurabh Wakhade <drsaurabh2018@gmail.com>
2023-04-02 16:22:40 +0530

    View Spefic data

---
+++ app/src/main/java/com/example/gpmpro/AdminViewSpecificData.java
@@ -0,0 +1,14 @@
+package com.example.gpmpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminViewSpecificData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view_specific_data);
+    }
+}
