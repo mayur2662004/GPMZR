ae6fa789a91ba5d192fc1ca6fcf0ddcf6664e9cc
Mayur wayzade <mayurwayzade26@gmail.com>
2023-04-02 16:22:33 +0530

    eee

---
+++ app/src/main/java/com/example/gpmpro/Student_Registretoin_Activity.java
@@ -0,0 +1,25 @@
+package com.example.gpmpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Student_Registretoin_Activity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_registretoin);
+
+
+
+
+
+
+    }
+}
