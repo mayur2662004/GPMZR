2ca564b53cbbe76d3c0e24cbec4e29dba2808ebf
Akshay Chaudhari <akshaychaudhari2042004@gmail.com>
2023-04-04 12:57:12 +0530

    saloni

--- app/src/main/java/com/example/gpmpro/Scan_activity.java
+++ app/src/main/java/com/example/gpmpro/Scan_activity.java
@@ -36,13 +36,7 @@ public class Scan_activity extends AppCompatActivity {
        bonafide.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-<<<<<<< HEAD
-//               ScanCode();
-=======
 
-//               ScanCode();
-
->>>>>>> bfeae045009a8761348ad194c612892cbd9e0db8
                startActivity(new Intent(getApplicationContext(),BonafiteCertificateForm.class));
            }
        });
